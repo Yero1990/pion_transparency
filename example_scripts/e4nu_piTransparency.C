90d99d606d135c09d89998d591409b9898372917
Carlos Yero <cyero@jlab.org>
2022-04-06 16:46:54 -0400

    initialized pion transparency code

---
+++ example_scripts/e4nu_piTransparency.C
@@ -0,0 +1,39 @@
+/*=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=
+
+  Author: Carlos Yero
+  Date Started: April 06, 2022
+  
+  Pion Transparency Analysis Code - CLAS12
+  (e4nu Collaboration) 
+
+
+=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:*/
+
+
+#include <cstdlib>
+#include <iostream>
+#include <chrono>
+#include <TFile.h>
+#include <TTree.h>
+#include <THStack.h>
+#include <TApplication.h>
+#include <TROOT.h>
+#include <TDatabasePDG.h>
+#include <TLorentzVector.h>
+#include <TH1.h>
+#include <TChain.h>
+#include <TCanvas.h>
+#include <TBenchmark.h>
+#include "clas12reader.h"
+#include "clas12writer.h"
+#include "HipoChain.h"
+
+using namespace clas12;
+using namespace std;
+
+void e4nu_piTransparency(){
+
+  TString inFile_data = "";
+  
+
+}
