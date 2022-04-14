35c2cbcd83f4328f11bbf706f2d3c78019dc0ab2
Carlos Yero <cyero@jlab.org>
2022-04-14 17:46:23 -0400

    minor changer to analyzer, and changed its name

--- analysis_scripts/e4nu_analyzer.C
+++ analysis_scripts/e4nu_analyzer.C
@@ -3,8 +3,13 @@
   Author: Carlos Yero
   Date Started: April 06, 2022
   
-  Pion Transparency Analysis Code - CLAS12
+  e4nu analyzer - CLAS12
   (e4nu Collaboration) 
+  
+  This code is used for sanity checks 
+  and analysis of Hall B run-group M (RGM) 
+  dataset 
+  
 
 
 =:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:=:*/
@@ -31,7 +36,7 @@
 using namespace clas12;
 using namespace std;
 
-void e4nu_piTransparency(){
+void e4nu_analyzer(){
   
   //hipo file location: /volatile/clas12/rg-m/LAr/prod1.0/dst/recon/015672/rec_clas_015672.evio.*.hipo  (for liquid Ar, for example)
   // It seems a single run number is divided into many .hipo files which must be combined together.
@@ -42,12 +47,26 @@ void e4nu_piTransparency(){
   // output file (to write leaf variables)
   TFile *outROOT = new TFile("e4nu_test_cyero.root","RECREATE");
 
+  // Define TTree and variables to write to it
+  TTree *tree = new TTree("tree","");
+
+  // Define variables to put in TTree here
+  // (remember, we want all particle types, and be able to make
+  // cuts on particle types, eaach sector, and either the FD or CND)
+
+  //---- DEFINE TTree branches ------
+  // tree->Branch("px",&px,"px/D");
+  //tree->Branch("py",&py,"px/D");
+  //tree->Branch("pz",&pz,"px/D");
+  //tree->Branch("chi2pid",&chi2pid,"chi2pid/D");
+
+  
   // load databases (check with Justin where are these located)
   clas12databases::SetCCDBLocalConnection("ccdb.sqlite");
   clas12databases::SetRCDBRootConnection("rcdb.root");
 
   //LH2 target (we can use to construct W and check.
-  TString inFile = "/work/clas12/rg-m/LH2/prod1.4/dst/recon/015024/rec_clas_015024.evio.000*.hipo";
+  TString inFile = "/work/clas12/rg-m/LH2/prod1.4/dst/recon/015024/rec_clas_015024.evio.0001*.hipo";
 
   // Chain multiple .hipo files
   clas12root::HipoChain chain;
@@ -112,6 +131,12 @@ void e4nu_piTransparency(){
 
       // get std::vector array of all final state particles per event
       auto particles = c12->getDetParticles();
+
+      for(auto& p : particles)
+	{
+	  // get all the info for every particle ttype here, and fill the tree outside
+	}
+	//Fill Tree Here !
       
       // get std::vector array of final state particles for the reaction of interest
       // if there are multiple of the same particle, e.g. 3 protons, but only want 1 proton,
@@ -171,6 +196,9 @@ void e4nu_piTransparency(){
 	}
 	
 
+
+	
+	
 	// Fill Histograms 
 	H_the ->Fill(th_e);
 	H_kf  ->Fill(p_e); 
@@ -190,72 +218,8 @@ void e4nu_piTransparency(){
       }
 
       
-      /*
-      double th_e = electrons[0]->getTheta();  //radians
       
-      double Ef = sqrt( pow(me,2) + pow(electrons[0]->par()->getP(), 2) );
-      double Q2 = 4.*Eb*Ef*pow(sin(th_e/2.), 2);
-      double nu = Eb - Ef;
-      double W = pow(MP,2) + 2*MP*nu - Q2;
-
-      if (electrons.size()==1 && protons.size()==1) {
-	cout << "Ef = " << Ef << endl;
-	cout << "Q2 = " << Q2 << endl;
-	cout << "nu = " << nu << endl;
-	cout << "W = " << W << endl;
-      }       
-      */
-      
-      /*
-      cout << "particles size: " << particles.size() << endl;
-      cout << "electrons size: " << electrons.size() << endl;
-      cout << "protons size: "   << protons.size() << endl;
-
-      cout << "proton mass: " << db->GetParticle(2212)->Mass() << endl;
-      cout << "electron mass: " << db->GetParticle(11)->Mass() << endl;  
-      */
-
-
-      // select ONLY 1 electron and 1 proton in final state: H(e,e'p) -> but about about everythin else?
-      // is it:  1 e- + 1 p + whatever else is detected?
-      /*
-      if (electrons.size()==1 && protons.size()==1){
-	
-	double th_e = electrons[0]->getTheta();  //radians                                                                                                             
-                                                                                                                                                                     
-	double Ef = sqrt( pow(me,2) + pow(electrons[0]->par()->getP(), 2) );                                                                                           
-	double Q2 = 4.*Eb*Ef*pow(sin(th_e/2.), 2);                                                                                                                     
-	double nu = Eb - Ef;                                                                                                                                           
-	double W = pow(MP,2) + 2*MP*nu - Q2;   
-	
-	cout << "Ef = " << Ef << endl;                                                                                                                               
-        cout << "Q2 = " << Q2 << endl;                                                                                                                               
-        cout << "nu = " << nu << endl;                                                                                                                               
-        cout << "W = " << W << endl;   
-
-	cout << "ELECTRONS == 1 ! ! ! ! " << endl;
-	// loop over pth element in particles array
-	for(int i=0;i<particles.size();i++){
-	  
-	  cout << "particle pid --> " << particles[i]->par()->getPid() << endl;	 
-	  cout << "particle time --> " << particles[i]->getTime() << endl;
-	  cout << "particle path --> " <<particles[i]->getPath() << endl;
-	  cout << "particle sector -->"	<< particles[i]->getSector() << endl;
-	  cout << "particle region --> " <<particles[i]->getRegion() << endl;
-	  cout << "particle theta --> " << particles[i]->getTheta() << endl;
-	  cout << "electron theta -->" << electrons[0]->getTheta() << endl;
-	  cout << "particle phi --> " << particles[i]->getPhi() << endl;
-
-	  cout << "px = " << particles[i]->par()->getPx() << endl;
-	  cout << "px_elec = " << electrons[0]->par()->getPx() << endl; 
-	  cout << "px = " << particles[i]->par()->getPy() << endl; 
-	  cout << "px = " << particles[i]->par()->getPz() << endl; 
-	  cout << "p = " << particles[i]->par()->getP() << endl; 
-
-	}
-      }
-      
-      */
+     
       /* NOTE: particles detected are separated into central detector (CD) and forward detector (FD) ( < 35 deg in-plane)
 	 From Justin Esteeves, currently the CD has much worse resolution and so one should look at events reconstructed from
 	 these detectors separately in order to compare how well reconstruction is done.
