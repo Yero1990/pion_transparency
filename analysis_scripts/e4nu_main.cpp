bd86eddc81bd43c74fdc7065a50e931a75d3b8f0
Carlos Yero <cyero@jlab.org>
2022-04-21 11:25:40 -0400

    added detected hadron option

--- analysis_scripts/e4nu_main.cpp
+++ analysis_scripts/e4nu_main.cpp
@@ -2,9 +2,9 @@
 #include "e4nu_analyzer.h"
 #include <iostream>
 
-int e4nu_main(TString inHIPO="", TString outROOT="", TString tgt="" ){
+int e4nu_main(TString inHIPO="", TString outROOT="", TString tgt="", TString det_h=""){
 
-  e4nu_analyzer e4nu(inHIPO, outROOT, tgt);
+  e4nu_analyzer e4nu(inHIPO, outROOT, tgt, det_h);
   e4nu.run_data_analysis();
   
   return 0;
--- analysis_scripts/run_analysis.sh
+++ analysis_scripts/run_analysis.sh
@@ -3,7 +3,8 @@
 inFile="/work/clas12/rg-m/LH2/prod1.4/dst/recon/015024/rec_clas_015024.evio.00001.hipo";
 outFile="test.root"
 target="LH2"
+detected="proton"
 
-CMD="clas12root -b \"e4nu_main.cpp(\\\"${inFile}\\\",\\\"${outFile}\\\", \\\"${target}\\\" )\" "
+CMD="clas12root -b \"e4nu_main.cpp(\\\"${inFile}\\\",\\\"${outFile}\\\", \\\"${target}\\\", \\\"{detected}\\\")\" "
 echo "executing command: $CMD" 
 eval ${CMD}
