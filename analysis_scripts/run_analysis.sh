0283f2bdc26b9c08dac969278768af3e35c5a5ed
Carlos Yero <cyero@jlab.org>
2022-04-22 09:34:28 -0400

    minor fix

--- analysis_scripts/run_analysis.sh
+++ analysis_scripts/run_analysis.sh
@@ -3,8 +3,8 @@
 inFile="/work/clas12/rg-m/LH2/prod1.4/dst/recon/015024/rec_clas_015024.evio.000*.hipo";
 outFile="rgm_LH2_015024.root"
 target="LH2"
-detected="proton"
+detected_hadron="proton"
 
-CMD="clas12root -b \"e4nu_main.cpp(\\\"${inFile}\\\",\\\"${outFile}\\\", \\\"${target}\\\", \\\"{detected}\\\")\" "
+CMD="clas12root -b \"e4nu_main.cpp(\\\"${inFile}\\\",\\\"${outFile}\\\", \\\"${target}\\\", \\\"${detected_hadron}\\\")\" "
 echo "executing command: $CMD" 
 eval ${CMD}
