05725472d8c3350af9bc704372408453be1d666d
Carlos Yero <cyero@jlab.org>
2022-05-06 19:23:39 -0400

    added remaining target files

--- analysis_scripts/run_analysis.sh
+++ analysis_scripts/run_analysis.sh
@@ -15,7 +15,7 @@ echo ""
 echo "-------------------------"
 echo "No Input Arguments Found ! "
 echo "Code Usage: ./run_analysis.sh [target] [detected_hadron] "
-echo "[target]: H, D"
+echo "[target]: H, D, He4, C12, Ca40, Ca48, Ar40, Sn120"
 echo "[detected_hadron]: p, pi+, pi-"
 echo "Example: ./run_analysis.sh D pi+"
 echo "--------------------------"
@@ -23,11 +23,23 @@ echo ""
 exit 1
 fi
 
-# select characteristis run based on selected target
+# select characteristis run based on selected target (selected runs have beam energy 5.98636 GeV)
 if [[ $target == "H" ]]; then
     inFile="/work/clas12/rg-m/LH2/prod1.4/dst/recon/015024/rec_clas_015024.evio.00001.hipo";
 elif [[ $target == "D" ]]; then
     inFile="/work/clas12/rg-m/LD2/prod1.4/dst/recon/015045/rec_clas_015045.evio.00001.hipo";
+elif [[ $target == "He4" ]]; then
+    inFile="/work/clas12/rg-m/LHe/prod1.4/dst/recon/015133/rec_clas_015133.evio.00001.hipo";
+elif [[ $target == "C12" ]]; then
+    inFile="/work/clas12/rg-m/Cx4/prod1.4/dst/recon/015188/rec_clas_015188.evio.00001.hipo";
+elif [[ $target == "Ca40" ]]; then
+    inFile="/work/clas12/rg-m/40Ca/prod1.4/dst/recon/015392/rec_clas_015392.evio.00001.hipo";
+elif [[ $target == "Ca48" ]]; then
+    inFile="/work/clas12/rg-m/48Ca/prod1.4/dst/recon/015832/rec_clas_015832.evio.00001.hipo";
+elif [[ $target == "Ar40" ]]; then
+    inFile="/work/clas12/rg-m/LAr/prod1.5/dst/recon/015795/rec_clas_015795.evio.00001.hipo";
+elif [[ $target == "Sn120" ]]; then
+    inFile="/work/clas12/rg-m/Snx4/prod1.4/dst/recon/015327/rec_clas_015327.evio.00001.hipo";
 fi
 
 # select detected hadron, and set output file name
